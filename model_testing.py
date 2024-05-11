e3c1c2cf02c073bb9a4951fe3cac5e2ecf47fb13
Jamie Milsom <04jmilsom@gmail.com>
2024-05-11 16:26:23 +0100

    initial commit - basic workflow setup

---
+++ model_testing.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO#
+import cv2
+import os
+
+model = YOLO('/home/jamiemilsom/Documents/bioinformatics/runs/detect/train4/weights/best.pt')
+
+images = ['cell_data/images/test/' + image for image in os.listdir('cell_data/images/test')]
+results = model(images)
+
+
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    probs = result.probs  # Probs object for classification outputs
+
+    im_array = result.plot()  
+    cv2.imshow('Image with Predicted Bounding Boxes', im_array)
+    if cv2.waitKey(0) == ord('q'):
+        break  
